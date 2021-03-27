df18f8c9a7fb8465c2746816f75e44b9731c29cc
SamuelPerrin <samwperrin@yahoo.com>
2021-03-27 11:21:09 -0500

    initial commit

---
+++ foodtrucktrackr/src/main/java/com/lambda/foodtrucktrackr/FoodtrucktrackrApplication.java
@@ -0,0 +1,13 @@
+package com.lambda.foodtrucktrackr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodtrucktrackrApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FoodtrucktrackrApplication.class, args);
+    }
+
+}
---
+++ foodtrucktrackr/src/test/java/com/lambda/foodtrucktrackr/FoodtrucktrackrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lambda.foodtrucktrackr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodtrucktrackrApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
